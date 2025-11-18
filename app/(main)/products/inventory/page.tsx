37757bb82fdc4f6e28a92159aae55bc390e30138
MISTAKELY <thereisamailforme@gmail.com>
2025-11-18 16:32:42 +0545

    analytics and customer biuld 2

--- app/(main)/products/inventory/page.tsx
+++ app/(main)/products/inventory/page.tsx
@@ -36,7 +36,7 @@ export default async function InventoryPage() {
   }, 0);
 
   return (
-    <div className="flex-1 space-y-4 p-4 md:p-8 pt-7">
+    <div className="flex-1 space-y-4 p-4 md:p-8 pt-6">
       <div className="flex items-center justify-between">
         <h2 className="text-3xl font-bold tracking-tight">Inventory</h2>
         <Button>Update Stock</Button>
