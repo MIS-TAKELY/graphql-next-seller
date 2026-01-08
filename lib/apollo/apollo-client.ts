03c880138c7d3096aeb7d98b7a7a085646316818
MISTAKELY <thereisamailforme@gmail.com>
2026-01-08 14:37:11 +0545

    few changes made on docker files

---
+++ lib/apollo/apollo-client.ts
@@ -0,0 +1,18 @@
+// lib/apollo/client.ts
+import { ApolloClient, createHttpLink } from "@apollo/client";
+import { useMemo } from "react";
+import { APOLLO_CONFIG, APOLLO_DEFAULT_OPTIONS } from "./config";
+
+const httpLink = createHttpLink({
+    uri: `${process.env.NEXT_PUBLIC_APP_URL}/api/graphql` || "http://localhost:3000/api/graphql",
+});
+
+export function useApolloClientWrapper() {
+    return useMemo(() => {
+        return new ApolloClient({
+            link: httpLink,
+            cache: APOLLO_CONFIG.cache,
+            defaultOptions: APOLLO_DEFAULT_OPTIONS,
+        });
+    }, []);
+}
