4b41eec37d6631fa13a1b80df8a453c171ad5f3a
MISTAKELY <thereisamailforme@gmail.com>
2026-02-08 17:03:57 +0545

    emoj error solved

--- utils/product/validateSteps.ts
+++ utils/product/validateSteps.ts
@@ -21,7 +21,7 @@ export const validateStep = (
         }
         // Allowed symbols: ALL special characters allowed.
         // Restriction: No Emojis.
-        const emojiRegex = /(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/;
+        const emojiRegex = /\p{Extended_Pictographic}/u;
         if (emojiRegex.test(formData.name)) {
           newErrors.name = "Emojis are not allowed in the title";
         }
@@ -44,7 +44,7 @@ export const validateStep = (
         }
 
         // No Emojis
-        const emojiRegex = /(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/;
+        const emojiRegex = /\p{Extended_Pictographic}/u;
         if (emojiRegex.test(description)) {
           newErrors.description = "Emojis are not allowed in the description";
         }
